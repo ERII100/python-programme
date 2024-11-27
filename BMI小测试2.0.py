ff967383f591e4332a1e69f13a16cbefa35b4b69
ERII100 <erii100thousand@gmail.com>
2024-11-27 18:10:51 +0800

    Add files via upload

---
+++ BMI小测试2.0.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+     height=float(input("请输入身高（m）："))
+     weight=float(input("请输入体重（kg）"))
+     if height>0 and weight>0:
+        BMI=weight/height**2
+        if 0<=BMI<18.5:
+         print("细狗")
+        elif BMI<=24.9:
+         print("人")
+        else:
+         print("猪")
+        break
+     elif height<=0 or weight<=0:
+         print("你是猪吗，数字都输不明白？")
+    except ValueError:
+        print("请输入数字！")
+
+       
